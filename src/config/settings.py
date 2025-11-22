6342845f6035a61a71b329bd302a239ad807c7b0
Greg Havekes <883845+havekes@users.noreply.github.com>
2025-11-21 17:42:32 -0800

    fix allow any db url in settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,11 +1,9 @@
-from pydantic import PostgresDsn
+from pydantic import AnyUrl, PostgresDsn
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    database_url: PostgresDsn = PostgresDsn(
-        "postgresql+asyncpg://retail-portfolio-user:password@localhost:5432/retail-portfolio"
-    )
+    database_url: AnyUrl = "postgresql+asyncpg://retail-portfolio-user:password@localhost:5432/retail-portfolio"
     echo_sql: bool = True
 
 
