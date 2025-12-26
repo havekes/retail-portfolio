6a929c887029c2c5d0a1da50bdf42e57bdd59d35
Reynald Barbeaut <reynald.barbeaut@gmail.com>
2025-12-26 13:44:18 -0500

    all front elements

--- frontend/src/lib/components/login-form.svelte
+++ frontend/src/lib/components/login-form.svelte
@@ -8,6 +8,7 @@
     FieldLabel,
     FieldDescription,
   } from "$lib/components/ui/field/index.js";
+<<<<<<< HEAD
   import { authService } from "$lib/services/authService";
   import { userStore } from "$lib/stores/userStore";
 
@@ -40,12 +41,17 @@
   }
 </script>
 
+=======
+  const id = $props.id();
+</script>
+>>>>>>> a29a120 (all front elements)
 <Card.Root class="mx-auto w-full max-w-sm">
   <Card.Header>
     <Card.Title class="text-2xl">Login</Card.Title>
     <Card.Description>Enter your email below to login to your account</Card.Description>
   </Card.Header>
   <Card.Content>
+<<<<<<< HEAD
 <<<<<<< HEAD
     <form onsubmit={handleSubmit}>
 =======
@@ -61,10 +67,18 @@
             required 
             bind:value={email} 
           />
+=======
+    <form>
+      <FieldGroup>
+        <Field>
+          <FieldLabel for="email-{id}">Email</FieldLabel>
+          <Input id="email-{id}" type="email" placeholder="m@example.com" required />
+>>>>>>> a29a120 (all front elements)
         </Field>
         <Field>
           <div class="flex items-center">
             <FieldLabel for="password-{id}">Password</FieldLabel>
+<<<<<<< HEAD
           </div>
           <Input 
             id="password-{id}" 
@@ -82,6 +96,18 @@
           </Button>
           <FieldDescription class="text-center">
             Don't have an account? <a href="/signup">Sign up</a>
+=======
+            <a href="##" class="ml-auto inline-block text-sm underline">
+              Forgot your password?
+            </a>
+          </div>
+          <Input id="password-{id}" type="password" required />
+        </Field>
+        <Field>
+          <Button type="submit" class="w-full">Login</Button>
+          <FieldDescription class="text-center">
+            Don't have an account? <a href="##">Sign up</a>
+>>>>>>> a29a120 (all front elements)
           </FieldDescription>
         </Field>
       </FieldGroup>
