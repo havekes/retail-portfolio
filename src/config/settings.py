4bfa19a09c2e3a7b05415c9814a2c0f7da753219
Greg Havekes <883845+havekes@users.noreply.github.com>
2026-01-01 16:03:59 -0800

    Improve readme

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,10 +1,12 @@
-from pydantic import AnyUrl, PostgresDsn
+from pydantic import PostgresDsn
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
     envrionement: str = "dev"
-    database_url: AnyUrl = "postgresql+asyncpg://retail-portfolio-user:password@localhost:5432/retail-portfolio"
+    database_url: PostgresDsn = PostgresDsn(
+        "postgresql+asyncpg://retail-portfolio-user:password@localhost:5432/retail-portfolio"
+    )
     echo_sql: bool = True
 
 
