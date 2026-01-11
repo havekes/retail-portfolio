3051dab7b68c135a6a689c7e1957ee1a24a2d2e1
Greg Havekes <883845+havekes@users.noreply.github.com>
2026-01-10 20:04:12 -0800

    move auth to routers and use svcs dep injection

---
+++ src/routers/auth.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from svcs.fastapi import DepContainer
+
+from src.database import get_db_session
+from src.repositories.sqlalchemy.sqlalchemy_user import SqlAlchemyUserRepository
+from src.schemas.auth import AuthResponse, LoginRequest, SignupRequest
+from src.services.auth import AuthService
+
+router = APIRouter(prefix="/api/auth")
+
+
+@router.post("/signup", response_model=AuthResponse)
+async def signup(request: SignupRequest, services: DepContainer):
+    auth_service = await services.aget(AuthService)
+    return await auth_service.signup(request.email, request.password)
+
+
+@router.post("/login", response_model=AuthResponse)
+async def login(request: LoginRequest, services: DepContainer):
+    auth_service = await services.aget(AuthService)
+
+    return await auth_service.login(request.email, request.password)
