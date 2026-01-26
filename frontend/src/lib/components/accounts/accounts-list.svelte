14c4ab36a56b7b1b13ca00a033b1fa7ef8381159
Greg Havekes <883845+havekes@users.noreply.github.com>
2026-01-25 16:24:29 -0800

    wip

--- frontend/src/lib/components/accounts/accounts-list.svelte
+++ frontend/src/lib/components/accounts/accounts-list.svelte
@@ -4,10 +4,10 @@
 	import * as DropdownMenu from '../ui/dropdown-menu';
 	import Button from '../ui/button/button.svelte';
 	import ChevronDown from '@lucide/svelte/icons/chevron-down';
-	import type { Account } from '@/types/account';
+	import { AccountGroupKeys, type Account } from '@/types/account';
 	import { onMount } from 'svelte';
 	import { accountService } from '@/services/accountService';
-	import { groupAccounts, type GroupBy } from '@/group';
+	import { group, groupAccounts, type GroupBy } from '@/group';
 
 	let accounts = $state<Promise<Set<Account>> | null>(new Set());
 	let selectionMode = $state(false);
@@ -22,6 +22,8 @@
 		institution: 'Institution',
 		accountType: 'Account type'
 	};
+	let groupByKey = null;
+	$: groupedAccounts = accounts ? group<Account, AccountGroupKeys>(accounts, groupByKey) : null;
 
 	onMount(() => {
 		accounts = accountService.getAccounts();
@@ -91,28 +93,27 @@
 			<Skeleton class="h-16 w-full rounded-md" />
 			<Skeleton class="h-16 w-full rounded-md" />
 		{:then}
-			{#if groupedAccounts}
-				{#await groupedAccounts}
-					<Skeleton class="h-16 w-full rounded-md" />
-					<Skeleton class="h-16 w-full rounded-md" />
-					<Skeleton class="h-16 w-full rounded-md" />
-				{:then groups}
-					{#each groups as group (group.key)}
-						<div class="space-y-3">
-							{#if groupBy !== 'none'}
-								<h3 class="text-xs font-semibold tracking-wide text-muted-foreground uppercase">
-									{group.label}
-								</h3>
-							{/if}
-							{#each group.accounts as account (account.id)}
-							    <AccountsListItem {account} {selectionMode} bind:selectedAccounts />
-							{/each}
-						</div>
-					{/each}
-				{:catch error}
-					<div class="error">Error: {error.message}</div>
-				{/await}
-			{/if}
+			{#await groupedAccounts}
+				<Skeleton class="h-16 w-full rounded-md" />
+				<Skeleton class="h-16 w-full rounded-md" />
+				<Skeleton class="h-16 w-full rounded-md" />
+			{:then groups}
+				{#each groups as group (group.key)}
+					<div class="space-y-3">
+						{#if groupBy !== 'none'}
+							<h3 class="text-xs font-semibold tracking-wide text-muted-foreground uppercase">
+								{group.label}
+								<!-- Call method to compute label using key value -->
+							</h3>
+						{/if}
+						{#each group.accounts as account (account.id)}
+		                       <AccountsListItem {account} {selectionMode} bind:selectedAccounts />
+						{/each}
+					</div>
+				{/each}
+			{:catch error}
+				<div class="error">Error: {error.message}</div>
+			{/await}
 		{:catch error}
 			<div class="error">Error: {error.message}</div>
 		{/await}
