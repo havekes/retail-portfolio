81d21a42bf2b995bfc58c79dae0db08278d69033
ReynaldBarbeaut <reynald.barbeaut@gmail.com>
2026-02-12 20:27:11 -0500

    add front to create portofolio (#37)

--- frontend/src/lib/components/accounts/accounts-list-item.svelte
+++ frontend/src/lib/components/accounts/accounts-list-item.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { Institution } from '@/types/account';
 	import Badge from '../ui/badge/badge.svelte';
+	import Checkbox from '../ui/checkbox/checkbox.svelte';
 	import EditableHeader from '../form/editable-header.svelte';
 	import { accountService } from '@/services/accountService';
 	import Skeleton from '../ui/skeleton/skeleton.svelte';
@@ -8,7 +9,7 @@
 	import * as Tooltip from '../ui/tooltip';
 	import { buttonVariants } from '../ui/button';
 
-	let { account } = $props();
+	let { account, selectionMode, selectedAccounts = $bindable() } = $props();
 
 	let accountName = $state(account.name);
 	let accountTotals = accountService.getAccountTotals(account.id);
@@ -18,9 +19,31 @@
 			accountService.renameAccount(account.id, accountName);
 		}
 	});
+
+	function toggleSelection() {
+		if (selectedAccounts.includes(account.id)) {
+			selectedAccounts = selectedAccounts.filter((id) => id !== account.id);
+
+			return;
+		}
+
+		selectedAccounts = [...selectedAccounts, account.id];
+	}
 </script>
 
 <div class="account-list-item group px flex space-x-4 rounded-lg bg-muted p-4">
+	{#if selectionMode}
+		<div class="flex border-r py-2 pr-4">
+			<div class="m-auto">
+				<Checkbox
+					class="cursor-pointer"
+					checked={selectedAccounts.includes(account.id)}
+					onCheckedChange={toggleSelection}
+				/>
+			</div>
+		</div>
+	{/if}
+
 	<div class="flex-1 space-y-2">
 		<EditableHeader bind:value={accountName} />
 		<div class="flex gap-x-2 text-sm">
--- frontend/src/lib/components/accounts/accounts-list.svelte
+++ frontend/src/lib/components/accounts/accounts-list.svelte
@@ -8,17 +8,51 @@
 	import { onMount } from 'svelte';
 	import { accountService } from '@/services/accountService';
 
-	let accounts: Promise<Array<Account>> | null = null;
+	let accounts = $state<Promise<Set<Account>> | null>(new Set());
+	let selectionMode = $state(false);
+	let selectedAccounts = $state<string[]>([]);
+	let isCreatePortfolioDisabled = $derived(selectionMode && selectedAccounts.length === 0);
 
 	onMount(() => {
 		accounts = accountService.getAccounts();
 	});
+
+	function toggleSelectionMode() {
+		selectionMode = true;
+	}
+
+	function cancelSelection() {
+		selectionMode = false;
+		selectedAccounts = [];
+	}
+
+	function createPortfolio() {
+		console.log(selectedAccounts);
+		selectionMode = false;
+		selectedAccounts = [];
+	}
+
+	function handleCreatePortfolioClick() {
+		if (selectionMode) {
+			createPortfolio();
+
+			return;
+		}
+
+		toggleSelectionMode();
+	}
 </script>
 
 <div class="accounts-list w-full space-y-4">
 	<div class="flex items-center border-b px-4 py-2">
 		<h2>Accounts</h2>
 		<div class="ms-auto">
+			{#if selectionMode}
+				<Button variant="outline" onclick={cancelSelection}>Cancel</Button>
+			{/if}
+			<Button disabled={isCreatePortfolioDisabled} onclick={handleCreatePortfolioClick}>
+			    {selectionMode ? 'Confirm Selection' : 'Create portfolio'}
+			</Button>
 			<DropdownMenu.Root>
 				<DropdownMenu.Trigger>
 					<Button variant="outline">
@@ -42,7 +76,7 @@
 			<Skeleton class="h-16 w-full rounded-md" />
 		{:then accounts}
 			{#each accounts as account (account.id)}
-				<AccountsListItem {account} />
+				<AccountsListItem {account} {selectionMode} bind:selectedAccounts />
 			{/each}
 		{:catch error}
 			<div class="error">Error: {error.message}</div>
