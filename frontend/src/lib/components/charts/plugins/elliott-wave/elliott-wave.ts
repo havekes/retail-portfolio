33618e732490548bb8d7dfdeb6e05310535e167b
Greg Havekes <883845+havekes@users.noreply.github.com>
2026-08-20 13:56:22 -0700

    Add chart Elliott Wave plugin primitive

---
+++ frontend/src/lib/components/charts/plugins/elliott-wave/elliott-wave.ts
@@ -0,0 +1,349 @@
+import type {
+	IChartApi,
+	ISeriesApi,
+	ISeriesPrimitive,
+	IPrimitivePaneView,
+	PrimitiveHoveredItem,
+	SeriesAttachedParameter,
+	SeriesType,
+	Time
+} from 'lightweight-charts';
+import type { ISubscription } from '../helpers/delegate';
+import type { DegreeWaveCount, WaveDegree, WavePoint } from '$lib/utils/finance/elliott-wave';
+import { DEGREE_STYLES, MAX_WAVE_POINTS } from './constants';
+import { MouseHandlers, type ProjectedPointWithTarget } from './mouse';
+import {
+	type DegreeRenderData,
+	type DrawingPreviewData,
+	type ElliottWaveRendererData,
+	type ProjectedWavePoint
+} from './pane-renderer';
+import { ElliottWavePaneView } from './pane-view';
+import { ElliottWaveState } from './state';
+
+export class ElliottWavesPrimitive implements ISeriesPrimitive<Time> {
+	private _chart: IChartApi | undefined = undefined;
+	private _series: ISeriesApi<SeriesType> | undefined = undefined;
+	private _requestUpdate: (() => void) | undefined = undefined;
+
+	private readonly _state: ElliottWaveState;
+	private readonly _mouseHandlers: MouseHandlers;
+	private readonly _paneViews: [ElliottWavePaneView];
+
+	private _currentCursor: string | null = null;
+
+	constructor(initialState?: {
+		activeDegree?: WaveDegree;
+		waves?: Record<WaveDegree, DegreeWaveCount | null>;
+	}) {
+		this._state = new ElliottWaveState();
+		this._mouseHandlers = new MouseHandlers();
+		this._paneViews = [new ElliottWavePaneView()];
+
+		if (initialState?.activeDegree) {
+			this._state.setActiveDegree(initialState.activeDegree);
+		}
+		if (initialState?.waves) {
+			this._state.setAllWaveCounts(initialState.waves);
+		}
+	}
+
+	public attached({ chart, series, requestUpdate }: SeriesAttachedParameter<Time>): void {
+		this._chart = chart;
+		this._series = series;
+		this._requestUpdate = requestUpdate;
+
+		this._mouseHandlers.attached(chart, series);
+		this._mouseHandlers.setDrawingMode(this._state.isDrawingMode());
+
+		this._state.wavePointsChanged().subscribe(() => {
+			this._requestUpdate?.();
+		}, this);
+
+		this._state.drawingModeChanged().subscribe((isDrawing) => {
+			this._mouseHandlers.setDrawingMode(isDrawing);
+			this._requestUpdate?.();
+		}, this);
+
+		this._state.degreeChanged().subscribe(() => {
+			this._requestUpdate?.();
+		}, this);
+
+		this._mouseHandlers.mouseMoved().subscribe(() => {
+			this._requestUpdate?.();
+		}, this);
+
+		this._mouseHandlers.pointHovered().subscribe((target) => {
+			this._state.setHoveredPoint(target);
+			this._requestUpdate?.();
+		}, this);
+
+		this._mouseHandlers.dragStarted().subscribe((target) => {
+			this._state.setDraggingPoint(target);
+			this._requestUpdate?.();
+		}, this);
+
+		this._mouseHandlers.pointDragged().subscribe((dragEvent) => {
+			this._state.updatePoint(
+				dragEvent.wave,
+				{ time: dragEvent.time, price: dragEvent.price },
+				dragEvent.degree
+			);
+			this._requestUpdate?.();
+		}, this);
+
+		this._mouseHandlers.dragEnded().subscribe(() => {
+			this._state.setDraggingPoint(null);
+			this._requestUpdate?.();
+		}, this);
+
+		this._mouseHandlers.chartClicked().subscribe((clickEvent) => {
+			if (this._state.isDrawingMode()) {
+				this._state.addPoint({
+					time: clickEvent.time,
+					price: clickEvent.price
+				});
+				this._requestUpdate?.();
+			}
+		}, this);
+
+		this._requestUpdate();
+	}
+
+	public detached(): void {
+		this._state.wavePointsChanged().unsubscribeAll(this);
+		this._state.drawingModeChanged().unsubscribeAll(this);
+		this._state.degreeChanged().unsubscribeAll(this);
+
+		this._mouseHandlers.mouseMoved().unsubscribeAll(this);
+		this._mouseHandlers.pointHovered().unsubscribeAll(this);
+		this._mouseHandlers.dragStarted().unsubscribeAll(this);
+		this._mouseHandlers.pointDragged().unsubscribeAll(this);
+		this._mouseHandlers.dragEnded().unsubscribeAll(this);
+		this._mouseHandlers.chartClicked().unsubscribeAll(this);
+
+		this._mouseHandlers.detached();
+		this._chart = undefined;
+		this._series = undefined;
+		this._requestUpdate = undefined;
+	}
+
+	public paneViews(): readonly IPrimitivePaneView[] {
+		return this._paneViews;
+	}
+
+	public updateAllViews(): void {
+		if (!this._chart || !this._series) {
+			this._paneViews[0].update(null);
+			return;
+		}
+
+		const rendererData = this._calculateRendererData();
+		this._updateCursor();
+		this._paneViews[0].update(rendererData);
+	}
+
+	public hitTest(): PrimitiveHoveredItem | null {
+		if (!this._currentCursor) return null;
+		return {
+			cursorStyle: this._currentCursor,
+			externalId: 'elliott-waves-primitive',
+			zOrder: 'top'
+		};
+	}
+
+	// State and Public API Accessors
+	public isDrawingMode(): boolean {
+		return this._state.isDrawingMode();
+	}
+
+	public setDrawingMode(enabled: boolean): void {
+		this._state.setDrawingMode(enabled);
+	}
+
+	public getActiveDegree(): WaveDegree {
+		return this._state.getActiveDegree();
+	}
+
+	public setActiveDegree(degree: WaveDegree): void {
+		this._state.setActiveDegree(degree);
+	}
+
+	public getWaveCount(degree?: WaveDegree): DegreeWaveCount | null {
+		return this._state.getWaveCount(degree);
+	}
+
+	public setWaveCount(degree: WaveDegree, waveCount: DegreeWaveCount | null): void {
+		this._state.setWaveCount(degree, waveCount);
+	}
+
+	public getAllWaveCounts(): Record<WaveDegree, DegreeWaveCount | null> {
+		return this._state.getAllWaveCounts();
+	}
+
+	public setAllWaveCounts(waves: Record<WaveDegree, DegreeWaveCount | null>): void {
+		this._state.setAllWaveCounts(waves);
+	}
+
+	public getPoints(degree?: WaveDegree): WavePoint[] {
+		return this._state.getPoints(degree);
+	}
+
+	public addPoint(
+		pointOrPrice: { time: Time; price: number } | number,
+		timeOrDegree?: Time | WaveDegree,
+		maybeDegree?: WaveDegree
+	): WavePoint {
+		if (typeof pointOrPrice === 'number') {
+			const price = pointOrPrice;
+			const time = timeOrDegree as Time;
+			const degree = maybeDegree;
+			return this._state.addPoint({ time, price }, degree);
+		} else {
+			const point = pointOrPrice;
+			const degree = timeOrDegree as WaveDegree | undefined;
+			return this._state.addPoint(point, degree);
+		}
+	}
+
+	public updatePoint(
+		wave: 1 | 2 | 3 | 4 | 5,
+		updateOrPrice: { time?: Time; price?: number } | number,
+		timeOrDegree?: Time | WaveDegree,
+		maybeDegree?: WaveDegree
+	): boolean {
+		if (typeof updateOrPrice === 'number') {
+			const price = updateOrPrice;
+			const time = timeOrDegree as Time | undefined;
+			const degree = maybeDegree;
+			return this._state.updatePoint(wave, { price, time }, degree);
+		} else {
+			const update = updateOrPrice;
+			const degree = timeOrDegree as WaveDegree | undefined;
+			return this._state.updatePoint(wave, update, degree);
+		}
+	}
+
+	public clearWave(degree?: WaveDegree): void {
+		this._state.clearWave(degree);
+	}
+
+	public wavePointsChanged(): ISubscription<{
+		degree: WaveDegree;
+		waveCount: DegreeWaveCount | null;
+	}> {
+		return this._state.wavePointsChanged();
+	}
+
+	public drawingModeChanged(): ISubscription<boolean> {
+		return this._state.drawingModeChanged();
+	}
+
+	public degreeChanged(): ISubscription<WaveDegree> {
+		return this._state.degreeChanged();
+	}
+
+	public destroy(): void {
+		this.detached();
+		this._state.destroy();
+	}
+
+	private _updateCursor(): void {
+		if (this._state.getDraggingPoint()) {
+			this._currentCursor = 'grabbing';
+		} else if (this._state.isDrawingMode()) {
+			this._currentCursor = 'crosshair';
+		} else if (this._state.getHoveredPoint()) {
+			this._currentCursor = 'grab';
+		} else {
+			this._currentCursor = null;
+		}
+	}
+
+	private _calculateRendererData(): ElliottWaveRendererData | null {
+		if (!this._chart || !this._series) return null;
+
+		const timeScale = this._chart.timeScale();
+		const series = this._series;
+		const degrees: WaveDegree[] = ['cycle', 'primary'];
+
+		const allProjectedPointsForMouse: ProjectedPointWithTarget[] = [];
+		const degreeRenderDataList: DegreeRenderData[] = [];
+
+		const hovered = this._state.getHoveredPoint();
+		const dragging = this._state.getDraggingPoint();
+		const activeDegree = this._state.getActiveDegree();
+
+		for (const degree of degrees) {
+			const config = DEGREE_STYLES[degree];
+			const waveCount = this._state.getWaveCount(degree);
+			const points = waveCount?.points ?? [];
+			const projectedPoints: ProjectedWavePoint[] = [];
+
+			for (const pt of points) {
+				const x = timeScale.timeToCoordinate(pt.time);
+				const y = series.priceToCoordinate(pt.price);
+
+				if (x !== null && y !== null) {
+					const isHovered = hovered?.degree === degree && hovered?.wave === pt.wave;
+					const isDragging = dragging?.degree === degree && dragging?.wave === pt.wave;
+
+					const projectedPoint: ProjectedWavePoint = {
+						wave: pt.wave,
+						x,
+						y,
+						time: pt.time,
+						price: pt.price,
+						isHovered,
+						isDragging
+					};
+
+					projectedPoints.push(projectedPoint);
+					allProjectedPointsForMouse.push({
+						degree,
+						wave: pt.wave,
+						x,
+						y,
+						originalPoint: pt
+					});
+				}
+			}
+
+			degreeRenderDataList.push({
+				degree,
+				config,
+				points: projectedPoints,
+				isActiveDegree: degree === activeDegree
+			});
+		}
+
+		this._mouseHandlers.setProjectedPoints(allProjectedPointsForMouse);
+
+		let preview: DrawingPreviewData | null = null;
+		if (this._state.isDrawingMode()) {
+			const activeConfig = DEGREE_STYLES[activeDegree];
+			const activeDegreeData = degreeRenderDataList.find((d) => d.degree === activeDegree);
+			const activePoints = activeDegreeData?.points ?? [];
+
+			if (activePoints.length < MAX_WAVE_POINTS) {
+				const nextWave = (activePoints.length + 1) as 1 | 2 | 3 | 4 | 5;
+				const lastPoint = activePoints.length > 0 ? activePoints[activePoints.length - 1] : null;
+				const lastMouse = this._mouseHandlers.getLastMousePosition();
+
+				preview = {
+					degree: activeDegree,
+					config: activeConfig,
+					nextWave,
+					lastPoint,
+					currentMouse:
+						lastMouse && lastMouse.insidePlotArea ? { x: lastMouse.x, y: lastMouse.y } : null
+				};
+			}
+		}
+
+		return {
+			degrees: degreeRenderDataList,
+			preview
+		};
+	}
+}
---
+++ frontend/src/lib/components/charts/plugins/elliott-wave/state.ts
@@ -0,0 +1,215 @@
+import type { Time } from 'lightweight-charts';
+import { Delegate, type ISubscription } from '../helpers/delegate';
+import type { DegreeWaveCount, WaveDegree, WavePoint } from '$lib/utils/finance/elliott-wave';
+import { MAX_WAVE_POINTS } from './constants';
+
+export interface PointTarget {
+	degree: WaveDegree;
+	wave: 1 | 2 | 3 | 4 | 5;
+}
+
+export class ElliottWaveState {
+	private _activeDegree: WaveDegree = 'cycle';
+	private _waveCounts: Record<WaveDegree, DegreeWaveCount | null> = {
+		cycle: null,
+		primary: null
+	};
+	private _isDrawingMode: boolean = false;
+	private _hoveredPoint: PointTarget | null = null;
+	private _draggingPoint: PointTarget | null = null;
+
+	private _wavePointsChanged: Delegate<{ degree: WaveDegree; waveCount: DegreeWaveCount | null }> =
+		new Delegate();
+	private _drawingModeChanged: Delegate<boolean> = new Delegate();
+	private _degreeChanged: Delegate<WaveDegree> = new Delegate();
+	private _hoverChanged: Delegate<PointTarget | null> = new Delegate();
+	private _dragChanged: Delegate<PointTarget | null> = new Delegate();
+
+	public wavePointsChanged(): ISubscription<{
+		degree: WaveDegree;
+		waveCount: DegreeWaveCount | null;
+	}> {
+		return this._wavePointsChanged;
+	}
+
+	public drawingModeChanged(): ISubscription<boolean> {
+		return this._drawingModeChanged;
+	}
+
+	public degreeChanged(): ISubscription<WaveDegree> {
+		return this._degreeChanged;
+	}
+
+	public hoverChanged(): ISubscription<PointTarget | null> {
+		return this._hoverChanged;
+	}
+
+	public dragChanged(): ISubscription<PointTarget | null> {
+		return this._dragChanged;
+	}
+
+	public getActiveDegree(): WaveDegree {
+		return this._activeDegree;
+	}
+
+	public setActiveDegree(degree: WaveDegree): void {
+		if (this._activeDegree !== degree) {
+			this._activeDegree = degree;
+			this._degreeChanged.fire(degree);
+		}
+	}
+
+	public isDrawingMode(): boolean {
+		return this._isDrawingMode;
+	}
+
+	public setDrawingMode(enabled: boolean): void {
+		if (this._isDrawingMode !== enabled) {
+			this._isDrawingMode = enabled;
+			this._drawingModeChanged.fire(enabled);
+		}
+	}
+
+	public getWaveCount(degree?: WaveDegree): DegreeWaveCount | null {
+		const targetDegree = degree ?? this._activeDegree;
+		return this._waveCounts[targetDegree] ?? null;
+	}
+
+	public setWaveCount(degree: WaveDegree, waveCount: DegreeWaveCount | null): void {
+		this._waveCounts[degree] = waveCount
+			? {
+					...waveCount,
+					points: [...(waveCount.points || [])]
+				}
+			: null;
+		this._wavePointsChanged.fire({ degree, waveCount: this._waveCounts[degree] });
+	}
+
+	public getAllWaveCounts(): Record<WaveDegree, DegreeWaveCount | null> {
+		return {
+			cycle: this._waveCounts.cycle
+				? { ...this._waveCounts.cycle, points: [...this._waveCounts.cycle.points] }
+				: null,
+			primary: this._waveCounts.primary
+				? { ...this._waveCounts.primary, points: [...this._waveCounts.primary.points] }
+				: null
+		};
+	}
+
+	public setAllWaveCounts(waves: Record<WaveDegree, DegreeWaveCount | null>): void {
+		this.setWaveCount('cycle', waves.cycle ?? null);
+		this.setWaveCount('primary', waves.primary ?? null);
+	}
+
+	public getPoints(degree?: WaveDegree): WavePoint[] {
+		return this.getWaveCount(degree)?.points ?? [];
+	}
+
+	public addPoint(point: { time: Time; price: number }, degree?: WaveDegree): WavePoint {
+		const targetDegree = degree ?? this._activeDegree;
+		const existingPoints = [...this.getPoints(targetDegree)];
+
+		if (existingPoints.length >= MAX_WAVE_POINTS) {
+			existingPoints.length = 0;
+		}
+
+		const nextWave = (existingPoints.length + 1) as 1 | 2 | 3 | 4 | 5;
+		const newPoint: WavePoint = {
+			wave: nextWave,
+			time: point.time,
+			price: point.price
+		};
+
+		existingPoints.push(newPoint);
+
+		const currentCount = this._waveCounts[targetDegree];
+		const updatedCount: DegreeWaveCount = {
+			points: existingPoints,
+			wave3Target: currentCount?.wave3Target ?? (nextWave === 3 ? point.price : null),
+			wave5Target: currentCount?.wave5Target ?? (nextWave === 5 ? point.price : null)
+		};
+
+		this._waveCounts[targetDegree] = updatedCount;
+		this._wavePointsChanged.fire({ degree: targetDegree, waveCount: updatedCount });
+
+		if (existingPoints.length >= MAX_WAVE_POINTS) {
+			this.setDrawingMode(false);
+		}
+
+		return newPoint;
+	}
+
+	public updatePoint(
+		wave: 1 | 2 | 3 | 4 | 5,
+		update: { time?: Time; price?: number },
+		degree?: WaveDegree
+	): boolean {
+		const targetDegree = degree ?? this._activeDegree;
+		const currentCount = this._waveCounts[targetDegree];
+		if (!currentCount || !currentCount.points) return false;
+
+		const pointIndex = currentCount.points.findIndex((p) => p.wave === wave);
+		if (pointIndex === -1) return false;
+
+		const points = [...currentCount.points];
+		const targetPoint = { ...points[pointIndex] };
+
+		if (update.time !== undefined) {
+			targetPoint.time = update.time;
+		}
+		if (update.price !== undefined) {
+			targetPoint.price = update.price;
+		}
+
+		points[pointIndex] = targetPoint;
+
+		const updatedCount: DegreeWaveCount = {
+			...currentCount,
+			points
+		};
+
+		this._waveCounts[targetDegree] = updatedCount;
+		this._wavePointsChanged.fire({ degree: targetDegree, waveCount: updatedCount });
+		return true;
+	}
+
+	public clearWave(degree?: WaveDegree): void {
+		const targetDegree = degree ?? this._activeDegree;
+		this._waveCounts[targetDegree] = null;
+		this._wavePointsChanged.fire({ degree: targetDegree, waveCount: null });
+	}
+
+	public setHoveredPoint(point: PointTarget | null): void {
+		const changed =
+			this._hoveredPoint?.degree !== point?.degree || this._hoveredPoint?.wave !== point?.wave;
+		if (changed) {
+			this._hoveredPoint = point;
+			this._hoverChanged.fire(point);
+		}
+	}
+
+	public getHoveredPoint(): PointTarget | null {
+		return this._hoveredPoint;
+	}
+
+	public setDraggingPoint(point: PointTarget | null): void {
+		const changed =
+			this._draggingPoint?.degree !== point?.degree || this._draggingPoint?.wave !== point?.wave;
+		if (changed) {
+			this._draggingPoint = point;
+			this._dragChanged.fire(point);
+		}
+	}
+
+	public getDraggingPoint(): PointTarget | null {
+		return this._draggingPoint;
+	}
+
+	public destroy(): void {
+		this._wavePointsChanged.destroy();
+		this._drawingModeChanged.destroy();
+		this._degreeChanged.destroy();
+		this._hoverChanged.destroy();
+		this._dragChanged.destroy();
+	}
+}
