e1114b7e99f6ac1b968954d8f545fcb5be5dba12
Greg Havekes <883845+havekes@users.noreply.github.com>
2026-09-17 16:10:55 -0700

    Add Holdings link to sidebar navigation

--- frontend/src/lib/components/layout/app-sidebar-actions.svelte
+++ frontend/src/lib/components/layout/app-sidebar-actions.svelte
@@ -2,6 +2,8 @@
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
 	import * as Kbd from '$lib/components/ui/kbd/index.js';
 	import Search from '@lucide/svelte/icons/search';
+	import ChartPie from '@lucide/svelte/icons/chart-pie';
+	import { resolve } from '$app/paths';
 	import { getContext } from 'svelte';
 
 	const toggleGlobalSearch = getContext<() => void>('toggleGlobalSearch');
@@ -18,6 +20,16 @@
 					<Kbd.Root>⌘</Kbd.Root>
 					<Kbd.Root>P</Kbd.Root>
 				</Kbd.Group>
+			</Sidebar.MenuButton>
+		</Sidebar.MenuItem>
+		<Sidebar.MenuItem>
+			<Sidebar.MenuButton tooltipContent="Holdings">
+				{#snippet child({ props })}
+					<a href={resolve('/holdings')} {...props}>
+						<ChartPie class="h-4 w-4 shrink-0" />
+						<span class="text-base">Holdings</span>
+					</a>
+				{/snippet}
 			</Sidebar.MenuButton>
 		</Sidebar.MenuItem>
 	</Sidebar.Menu>
